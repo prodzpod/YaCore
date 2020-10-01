53d3e1be54c7a2b3a4635a2f2247a5ba99ea403c
prodzpod <product0339@gmail.com>
2020-10-02 02:51:46 +0900

    remove original recipe if active

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # YaCore
-a Mod made for the YaCraft modpack.
+a Mod made for the YaCraft modpack.   
+you are free to include this mod in any of your modpack/stuff, and insert liscense stuff here.   
+All contents are customizable.
+
+## Contents
+### World Generation
+* Custom end city/nether fortress rates   
+&nbsp;&nbsp;&nbsp;&nbsp;Somehow mods that does this doesnt really exist.
+### Thaumic Augmentation
+* Crafting Impetus Jewel with *Actual Impetus*   
+&nbsp;&nbsp;&nbsp;&nbsp;Craft a Void Seed(can change in config) with Impetus Conductor containing more than 100(can change in config) Impetus to craft 2(can change in config) Impetus Jewels. you get the Impetus Conductor back with the Impetus value subtracted.
+
+## (Maybe) todo?
+* Global Infusion Instability Modifier   
+&nbsp;&nbsp;&nbsp;&nbsp;Add a coremod that multiplies infusion instability by config amount
+* Expanded Eldritch Cluster Support   
+&nbsp;&nbsp;&nbsp;&nbsp;Inspired from JAOPCA - for Thaumic Wonders
--- src/main/java/kr/o_r/prodzpod/yacore/init/InitHandler.java
+++ src/main/java/kr/o_r/prodzpod/yacore/init/InitHandler.java
@@ -9,13 +9,18 @@ import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import thaumcraft.api.ThaumcraftApi;
+import thecodex6824.thaumicaugmentation.api.ThaumicAugmentationAPI;
 
 public class InitHandler {
-    @SubscribeEvent
+
+    @SubscribeEvent(priority = EventPriority.LOW)
     public void registerRecipes(RegistryEvent.Register<IRecipe> event) {
         if (Loader.isModLoaded("thaumicaugmentation") && YaCoreConfig.compat.impetusAmount != 0) {
             System.out.println("TA SPECIFICATION DETECTED");
+            ThaumcraftApi.getCraftingRecipes().remove(new ResourceLocation(ThaumicAugmentationAPI.MODID, "impetus_jewel"));
             event.getRegistry().register(new CustomTAImpetusRecipe().setRegistryName(new ResourceLocation(Reference.MODID, "impetus_jewel_new")));
         }
     }
